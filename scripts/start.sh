c06738334f4257af48fa19a567ecec2954d11d8d
MaxFrolow <maxfrolow777@gmail.com>
2025-07-19 13:58:33 +0300

    Fixed errors start.sh and added docker create network

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,18 +1,33 @@
-#!/bin/bash 
+#!/bin/bash
 
 if [[ $# != 1 ]]; then
-	echo "Please specify 1 compoent to start"
-	exit 1
+        echo "Please specify 1 compoent to start"
+        exit 1
 fi
 
-if [[ $1 == 'db' ]]lthen
-	cd $DB_DIR
-	docker compose up
+
+
+
+
+#Check if docker network exists
+        if ! docker network ls --format '{{.Name}}' | grep -wq market-simulator-net; then
+                echo "Network does not exist, creating..."
+                docker network create my-shared-network
+        else
+                echo "Network already exists."
+        fi
+
+
+
+if [[ $1 == 'db' ]];then
+
+        cd $DB_DIR
+        docker compose up
 else
-	if [[ -f $START_DIR/$1 ]];then
-		cd $SIM_DIR/$1;docker-compose up
-	else
-		echo "Simulator haven't been found"
-		exit 1
-	fi
+        if [[ -f $START_DIR/$1 ]];then
+                cd $SIM_DIR/$1;docker-compose up
+        else
+                echo "Simulator haven't been found"
+                exit 1
+        fi
 fi
